aea10043ddadeaca9210799cbd2ce7ddf153aa66
mokastinkey <mocastinkey@gmail.com>
2021-10-30 22:53:59 +0900

    open click classremove

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+{
+  const open = document.getElementById('open');
+  const close = document.getElementById('close');
+  const modal = document.getElementById('modal');
+  const mask = document.getElementById('mask');
+
+  open.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+  });
+}
